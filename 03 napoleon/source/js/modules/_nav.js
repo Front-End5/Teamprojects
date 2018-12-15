4d0fce1f591c5d811aaeb43517eabbd0cb21c809
alexhateful <alexhateful@gmail.com>
2018-12-15 18:37:17 +0200

    add homepage mobile

--- 03 napoleon/source/js/modules/_nav.js
+++ 03 napoleon/source/js/modules/_nav.js
@@ -1,1 +1,8 @@
 // код менюшки навигации
+const burger = $('.js-burger'),
+        nav = $('.js-nav');
+
+$(burger).on('click', function () {
+    $(this).toggleClass('is-active');
+    $(nav).slideToggle('fast');
+})
